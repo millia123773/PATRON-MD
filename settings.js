32819942a390b5241e5d4c08367266794ea14a60
millia123773 <miliberry175@gmail.com>
2025-01-13 01:58:23 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,8 +1,8 @@
-global.bankname = "MONIEPOINT"
+global.bankname = "OPAY"
 
-global.banknumber = "8133729715"
+global.banknumber = "8062334435"
 
-global.bankowner = "FADARE"
+global.bankowner = "AMINA"
 
 global.autoTyping = false //auto tying in gc (true to on, false to off)
 global.autoRecord = false //auto recording (true to on, false to off)
